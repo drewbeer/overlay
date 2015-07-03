296313f498d8239470beb3d7400ee264d923dd3c
root <vcall07-01@safesoft.io>
2015-07-03 00:56:58 -0700

    fixed the pax

--- net-misc/sangoma-nca/files/pax_fix.sh
+++ net-misc/sangoma-nca/files/pax_fix.sh
@@ -1,13 +1,13 @@
 echo "Creating Headers"
-paxctl -c /opt/sangoma/Sangoma_NetBorderCallAnalyzer/bin/netborder-call-analyzer-engine
-paxctl -c /opt/sangoma/Sangoma_NetBorderCallAnalyzer/bin/netborder-call-analyzer-service
-paxctl -c /opt/sangoma/Sangoma_NetBorderCallAnalyzer/bin/netborder-oam-cmd
-paxctl -c /opt/sangoma/Sangoma_NetBorderCallAnalyzer/bin/netborder-python
-paxctl -c /opt/sangoma/Sangoma_NetBorderCallAnalyzer/bin/netborder-rm
+paxctl -c /opt/sangoma/nbca/bin/netborder-call-analyzer-engine
+paxctl -c /opt/sangoma/nbca/bin/netborder-call-analyzer-service
+paxctl -c /opt/sangoma/nbca/bin/netborder-oam-cmd
+paxctl -c /opt/sangoma/nbca/bin/netborder-python
+paxctl -c /opt/sangoma/nbca/bin/netborder-rm
 
 echo "Disabling memory pax check"
-paxctl -m /opt/sangoma/Sangoma_NetBorderCallAnalyzer/bin/netborder-call-analyzer-engine
-paxctl -m /opt/sangoma/Sangoma_NetBorderCallAnalyzer/bin/netborder-call-analyzer-service
-paxctl -m /opt/sangoma/Sangoma_NetBorderCallAnalyzer/bin/netborder-oam-cmd
-paxctl -m /opt/sangoma/Sangoma_NetBorderCallAnalyzer/bin/netborder-python
-paxctl -m /opt/sangoma/Sangoma_NetBorderCallAnalyzer/bin/netborder-rm
+paxctl -m /opt/sangoma/nbca/bin/netborder-call-analyzer-engine
+paxctl -m /opt/sangoma/nbca/bin/netborder-call-analyzer-service
+paxctl -m /opt/sangoma/nbca/bin/netborder-oam-cmd
+paxctl -m /opt/sangoma/nbca/bin/netborder-python
+paxctl -m /opt/sangoma/nbca/bin/netborder-rm
